31276c1c4d256d1c2fa5d8fdf11d92dfb52a0797
Lester <lehzter@gmail.com>
2024-01-22 10:21:48 +0800

    temperature convertiion

---
+++ index.js
@@ -0,0 +1,20 @@
+const textbox = document.getElementById("textBox");
+const fartenheit = document.getElementById("toFartenheit");
+const celcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+
+    if(toFartenheit.checked){
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1)+ "°"
+    }else if(toCelcius.checked){
+        temp = Number(textbox.value);
+        temp = (temp -32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°"
+    }else{
+        result.textContent = "Select a Unit"
+    }
+}
